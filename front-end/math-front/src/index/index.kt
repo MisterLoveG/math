d5841d599201fe21bbafda2a499b08dc003248db
Rafael Ferreira Rocha <rafael.ferreira.rocha0@gmail.com>
2018-01-06 01:19:20 -0200

    join with front end test case

---
+++ front-end/math-front/src/index/index.kt
@@ -0,0 +1,16 @@
+package index
+
+import kotlinext.js.*
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import react.dom.*
+import kotlin.browser.*
+
+fun main(args: Array<String>) {
+    requireAll(require.context("src", true, js("/\\.css$/")))
+    render(document.getElementById("root")) {
+        welcome("Rafael")
+    }
+}
