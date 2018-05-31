fa7730d8e29dc15aa2b28e1c90608e69fe4623a9
rafael.ferreira <rafael.ferreira.rocha0@gmail.com>
2018-05-31 00:54:06 -0300

    add swagger2 and Spring cache

--- back-end/src/main/kotlin/com/xp/math/MathApplication.kt
+++ back-end/src/main/kotlin/com/xp/math/MathApplication.kt
@@ -2,10 +2,30 @@ package com.xp.math
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableCaching
 class MathApplication
 
 fun main(args: Array<String>) {
     runApplication<MathApplication>(*args)
+}
+
+@Bean
+fun api(): Docket {
+    return Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
 }
