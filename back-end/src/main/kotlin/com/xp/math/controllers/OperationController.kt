364a1e5f623d120da96d53599c6c8626f46b0763
rafael.ferreira <rafael.ferreira.rocha0@gmail.com>
2018-05-31 00:57:44 -0300

    Improve controller to get params

--- back-end/src/main/kotlin/com/xp/math/controllers/OperationController.kt
+++ back-end/src/main/kotlin/com/xp/math/controllers/OperationController.kt
@@ -1,19 +1,31 @@
 package com.xp.math.controllers
+
 import br.com.xp.operation.Valuable
 import com.xp.math.operation.OperationFactory
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
 
 
 @RestController
-class OperationController{
+class OperationController {
 
-    @GetMapping(value= "/operation", produces = ["application/json"])
-    fun getDescription(): Valuable =OperationFactory
-            .Build(0,10,1)
+    @GetMapping(value = ["/operation"], produces = ["application/json"])
+    @Cacheable("equation")
+    fun getAllOperations(
+            @RequestParam("min") min: Int,
+            @RequestParam("max") max: Int,
+            @RequestParam("equations") @Min(1) @Max(100) counts: Int
+    ): ArrayList<Valuable> = OperationFactory
+            .Builder(min, max, counts)
             .addSubtraction()
             .addSum()
+            .addDivision()
+            .addMultipler()
             .build()
             .generateOperations()
-            .get(0)
 }
